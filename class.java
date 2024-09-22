6ddd62e87c63c701c18944f367cfc383293d00a9
AktanZhusupbekov <150648923+AktanZhusupbekov@users.noreply.github.com>
2024-09-22 14:30:17 -0400

    Update class.java

--- class.java
+++ class.java
@@ -1,31 +1,31 @@
 public class Building {
     private String foundation;
-    private String roof;
+    private int emergencyExits; 
     private String thermalInsulation;
     private String facade;
 
     public Building() {
         this.foundation = "";
-        this.roof = "";
+        this.emergencyExits = 0;
         this.thermalInsulation = "";
         this.facade = "";
     }
 
-    public Building(String foundation, String roof, String thermalInsulation, String facade) {
+    public Building(String foundation, int emergencyExits, String thermalInsulation, String facade) {
         this.foundation = foundation;
-        this.roof = roof;
+        this.emergencyExits = emergencyExits;
         this.thermalInsulation = thermalInsulation;
         this.facade = facade;
     }
 
     public void displayInfo() {
         System.out.println("Foundation: " + foundation);
-        System.out.println("Roof: " + roof);
+        System.out.println("Number of Emergency Exits: " + emergencyExits);
         System.out.println("Thermal Insulation: " + thermalInsulation);
         System.out.println("Facade: " + facade);
     }
 
-    public double ESC() {
+    public double calculateEstimatedCost() {
         double baseCost = 1000000;
         if (foundation.equals("concrete")) {
             baseCost += 50000;
